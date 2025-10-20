0e85a5a277f5ae8f71712f4e5fd1e262f0f418f7
ti-jumbo <jumbo.ti@jumboalimentos.com.br>
2025-10-20 15:36:58 -0300

    first commit

---
+++ src/main/java/com/oiis/libs/security/sso/base_server_sso_client_security_config/BaseServerSSOClientSecurityConfig.java
@@ -0,0 +1,87 @@
+package com.oiis.libs.security.sso.base_server_sso_client_security_config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Class that represents base sso client configuration.
+ *
+ * @author aalvencarvz1
+ * @version 1.0.0
+ */
+@Configuration
+@EnableWebSecurity
+public class BaseServerSSOClientSecurityConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(BaseServerSSOClientSecurityConfig.class);
+
+    /**
+     * default filter check
+     *
+     * @param baseSsoEndpoint the endpoint of sso
+     * @return instance of this base filter check
+     */
+    @Bean
+    public BaseServerSsoClientAuthenticationFilterCheck baseSsoAuthenticationFilterCheck(@Value("${sso.base-endpoint}") String baseSsoEndpoint) {
+        return new BaseServerSsoClientAuthenticationFilterCheck(baseSsoEndpoint);
+    }
+
+    @Value("${app.security.public-endpoints}")
+    private List<String> publicEndpoints;
+
+    /**
+     * Configure cors
+     *
+     * @return the cors configuration
+     */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*"); // ou coloque a origem específica do seu front
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(false); // ou true, se usar cookies/sessão
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+    /**
+     * filter chain
+     *
+     * @param http the http security request
+     * @param baseSsoAuthenticationFilterCheck the base client filter check
+     * @return the security filter chain
+     * @throws Exception throw exception if error on http build
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, BaseServerSsoClientAuthenticationFilterCheck baseSsoAuthenticationFilterCheck) throws Exception {
+        logger.debug("public endpoints: {} {}", Arrays.toString(publicEndpoints.toArray(new String[0])),publicEndpoints);
+        http.csrf(csrf -> csrf.disable()) // desabilita CSRF no novo padrão
+                .cors(Customizer.withDefaults())             // habilita CORS (pode customizar aqui)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(publicEndpoints.toArray(new String[0]))
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .addFilterBefore(baseSsoAuthenticationFilterCheck, org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class);
+        ;
+        return http.build();
+    }
+}
